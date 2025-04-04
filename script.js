99512730962f8ac5ab350d018b58f996d4bbf589
Exserlot <122360989+Exserlot@users.noreply.github.com>
2025-04-04 22:59:34 +0700

    [Initialize]

---
+++ script.js
@@ -0,0 +1,204 @@
+const song = {
+    name: 'Long Night pt.||',
+    src: 'assets/song.mp3',
+    cover: 'assets/song.jpg',
+    artist: 'JPB',
+    lyrics:
+    `0 | สวัสดีนะปอ\n` +
+    `3 | วันนี้เค้ามาอธิบายความรู้สึกของเค้านะ\n` +
+    `6 | เธออาจถามเค้าว่า ถ้าเธอไม่ไปจากเค้า \n` +
+    `9 | เค้าจะบอกแต่ละอย่างให้เธอฟังมั้ย \n` +
+    `12 | แต่วันนี้เค้าแค่อยากจะมาบอกเธอนะ\n` +
+    `15 | แค่ได้บอกอยากให้เธออ่านให้จบ\n` +
+    `18 | ความฝันของเค้าคือ เค้าอยากอยู่กับเธอ\n` +
+    `21 | อยากแต่งงานอายุสัก 28 คงไม่แก่ไป\n` +
+    `24 | งานไม่ต้องใหญ่ แล้วก็อยากเลี้ยงสัตว์\n` +
+    `27 | อยากมีลูกแต่ไม่รู้จะเลี้ยงไหวมั้ย 5555\n` +
+    `30 | มีบ้านที่ขนาดกลางๆ อยู่ใกล้ๆห้าง\n` +
+    `33 | มีโรงพยาบาล โรงเรียนดีๆ ใกล้เพื่อน ด้วยก็ดี\n` +
+    `37 | อยากมีรถสวยๆ สักคัน\n` +
+    `40 | จริงๆ เค้าชอบ camry ชอบรุ่นนี้มาก ทรงมันดูไฮเทค\n` +
+    `44 | ถ้ามีตังอีกก็อยากมีมอไซค์ด้วยแบบ Classic\n` +
+    `47 | มีงานที่เป็นแบบ Hybrid\n` +
+    `50 | เผื่อวันไหนอยากอยู่บ้านกับเธอ หรือต้องอยู่ดูแลเธอ\n` +
+    `54 | อันนี้คือความฝันของเค้ามีแค่นี้เลย\n` +
+    `57 | เค้าไม่รู้ว่ามันตรงกับเธอมั้ย\n` +
+    `60 | แต่เค้าอยากทำทุกอย่างนี้กับเธอนะ\n` +
+    `63 | หลังจากนี้จะเป็นความรู้สึกของเค้านะ\n` +
+    `66 | เค้ารักเธอ รักมากๆ แล้วก็ทั้งห่วงและก็หวงมากๆ\n` +
+    `70 | เค้าอยาก Support เธอไปให้ได้ตลอด\n` +
+    `73 | อยากเดินห้างกับเธออยากไปเที่ยวกับเธอ\n` +
+    `76 | อยากเติบโตไปพร้อมกับเธอ\n` +
+    `79 | ชอบเวลาโดนเธองอแงใส่ หรือเวลาที่เธอมาคลอเคลียเค้า\n` +
+    `83 | เวลาที่เราได้ออกกำลังกายด้วยกัน ได้ใช้ชีวิตด้วยกัน\n` +
+    `87 | เค้ามีความสุขกับช่วงเวลานั้นๆมากเลยอะ\n` +
+    `90 | เธอเป็นคนที่เวลาเค้าหมดแรง หมดไฟ\n` +
+    `93 | รอยยิ้มของเธอ กอดของเธอ เป็นสิ่งที่สามารถเติมพลังให้เค้าได้ตลอดเลย\n` +
+    `98 | มันดูอบอุ่น และจริงใจมากๆ\n` +
+    `101 | อยากขอบคุณเธอที่เป็นกำลังใจให้เค้ามาตลอด\n` +
+    `105 | เค้าเป็นเค้าในตอนนี้ได้ก็เพราะเธอเลยอะ\n` +
+    `108 | เค้ามีเธออยู่ในทุกช่วงๆ เวลาของเค้าเลยนะ\n` +
+    `112 | แล้วก็ ตั้งแต่วันนั้นอะที่เธอมาเปิดใจกับเค้า\n` +
+    `115 | เธออยากลองเปิดใจดู เอาจริงๆเค้าก็เสียใจแหละ\n` +
+    `119 | เค้าไม่สบายใจเลย แต่เค้าก็รักเธอ\n` +
+    `122 | วันนั้นเค้าเสียใจมากแต่ก็ไม่อยากร้องไห้ออกมา\n` +
+    `126 | จนเค้าเก็บความรู้สึกนั้นไม่บอกใคร\n` +
+    `129 | จนผ่านไปสองวันเค้าก็ไม่สามารถเก็บมันไว้ได้\n` +
+    `132 | เค้าทำงานอยู่แล้วอยู่ แล้วเค้าก็ร้องไห้เลย\n` +
+    `135 | มันเต็มอกอะ มันไม่สามารถเก็บไว้ได้\n` +
+    `138 | นี้เป็นอีกครั้งที่อยู่ๆ เค้าก็ปล่อยโฮวออกมาเลยเหมือนตอนชินตะหายไป\n` +
+    `143 | หลังจากนั้นเค้าก็กลายเป็นคนซึมๆไป\n` +
+    `146 | อะไรหลายๆอย่างมันเข้ามาในหัวเค้าไปหมดเลย\n` +
+    `150 | เค้าอยากโทรไปหาเธอเลยแต่เค้ารู้ว่าเธอยุ่งมากๆ\n` +
+    `154 | แล้วก็ไม่อยากให้เธอต้องมาคิดมากกับเค้า\n` +
+    `157 | หลังจากนี้จะเป็นคำขอโทษ หรือคำขอ ของเค้านะ\n` +
+    `161 | เค้าขอโทษที่เค้าไม่เคยรู้ ความรู้สึกนั้นของเธอเลย\n` +
+    `165 | เค้าไม่รู้จะทำยังไงให้เธอหายโกรธเค้า\n` +
+    `168 | เพราะถ้ามันไม่เกิดเรื่องแบบนี้\n` +
+    `171 | เค้าคงต้องรอเธอมาบอกให้เปิดใจคุยกันเรื่องอนาคตแบบจริงๆ จังๆ กัน \n` +
+    `176 | เค้าถึงจะบอกเรื่องพวกนั้นไป\n` +
+    `179 | เค้าขอโทษกับทุกๆอย่างเลยนะ ที่ทำให้เธอเสียใจ\n` +
+    `182 | ไม่สบายใจ หรือคาใจก็ตาม เค้าขอโทษนะ\n` +
+    `186 | เค้าอยากไปต่อกับเธอจริงๆ\n` +
+    `189 | เค้าอยากจะขอโอกาสจากเธอ\n` +
+    `192 | เค้าไม่รู้ว่าเธอจะยังให้โอกาสเค้าอยู่มั้ย\n` +
+    `195 | เค้าอยากปรับให้เราเข้ากันได้นะ\n` +
+    `198 | เค้าไม่รู้ด้วยว่าตอนเธอได้อ่านอันนี้มันจะช้าไปมั้ย\n` +
+    `202 | เค้าพยายามตั้งใจทำอะไม่รู้ว้าเธอจะชอบมั้ยด้วย\n` +
+    `206 | เค้าไม่ขอสัญญาว่าเค้าจะทำให้เธอมีความสุขที่สุด\n` +
+    `210 | แต่เค้าสัญญาว่าเค้าจะเป็นคนที่พยายามทำให้เธอมีความสุขมากที่สุด\n` +
+    `215 | และจะพยายามเติบโตให้มากกว่านี้ครับ\n` +
+    `218 | และก็จะพยายาม Deep talk กับเธอบ่อยๆ นะ\n` +
+    `222 | เค้าเปิดใจคุยกับพ่อ กับแม่แล้วนะ\n` +
+    `225 | ทั้งเรื่องที่เกิดขึ้นกับเธอ เรื่องอนาคตเค้า\n` +
+    `228 | สิ่งที่เค้าคิด ความรู้สึกของเค้า\n` +
+    `231 | แล้วก็อะไรหลายๆ อย่างเลย\n` +
+    `234 | เค้าพร้อมออกไปใช้ชีวิตกับเธอแล้วนะ\n` +
+    `237 | เค้าขอกลับไปทำตามสัญญาที่เค้าเคยให้ไว้กับแม่เธอนะ ที่จะคอยดูแลเธอไปตลอด\n` +
+    `242 | เค้ารักเธอมากๆเลย\n` +
+    `245 | เค้าอาจพูดคำนี้บ่อยแต่ คำนี้เป็นคำที่เค้ารู้สึกมาจากหัวใจเค้าจริงๆ\n` +
+    `249 | และมันไม่เคยเปลี่ยนไปเลย\n` +
+    `252 | นี้เป็นความรู้สึก คำขอ ความต้องการ และความคิดเค้าบางส่วนนะ\n`+
+    `257 | ถ้าเธออยากรู้อะไรอีกก็ถามเค้าได้เลย\n`
+};
+var audioPlayer = document.getElementsByTagName('audio')[0];
+var card = document.querySelector('.card');
+var card_body = document.querySelector('.card-body');
+var play = document.querySelector('.play');
+var pause = document.querySelector('.pause');
+var mute = document.querySelector('.mute');
+var unmute = document.querySelector('.unmute');
+var progress_bar = document.querySelector('.progress_bar');
+var progress_bar_wrap = document.querySelector('.progress_bar_wrap');
+var lyrics = document.querySelector('.lyrics');
+var cover = document.querySelector('.cover');
+var song_wrapper = document.querySelector('.song_wrapper');
+
+
+card_body.addEventListener('click', function () {
+    if (audioPlayer.paused) {
+        playMusic();
+    }
+    else {
+        pauseMusic();
+    }
+});
+
+const backgrounds = ['assets/img.png', 'assets/img2.jpg', 'assets/img3.jpg', 'assets/img4.jpg', 'assets/img5.jpg', 'assets/img18.jpg', 'assets/img6.jpg', 'assets/img7.jpg', 'assets/img8.jpg', 'assets/img9.jpg', 'assets/img10.jpg', 'assets/img17.jpg', 'assets/img11.jpg', 'assets/img12.jpg', 'assets/img13.jpg', 'assets/img19.jpg', 'assets/img14.jpg', 'assets/img15.jpg', 'assets/img16.webp' ];
+
+let currentIndex = 0;
+let backgroundInterval = null;  // ตัวแปรสำหรับเก็บการตั้งเวลา interval
+function changeBackground() {
+    currentIndex = (currentIndex + 1) % backgrounds.length;
+    console.log("Changing background to:", backgrounds[currentIndex]);  // ลองเช็คว่า index เปลี่ยนไหม
+    card.style.backgroundImage = `url('${backgrounds[currentIndex]}')`;
+}
+
+play.addEventListener('click', function () {
+    playMusic();
+});
+
+function playMusic() {
+    audioPlayer.play();
+    lyrics.style.display = 'flex';
+    play.style.display = 'none';
+    pause.style.display = 'block';
+
+    if (progress_bar.clientWidth === progress_bar_wrap.clientWidth)
+    {
+        card.style.backgroundImage = `url('assets/img.png')`;
+    }
+
+    backgroundInterval = setInterval(changeBackground, 5000);
+}
+
+pause.addEventListener('click', function () {
+    pauseMusic();
+});
+
+function pauseMusic() {
+    audioPlayer.pause();
+    pause.style.display = 'none';
+    play.style.display = 'block';
+
+    clearInterval(backgroundInterval);
+}
+
+mute.addEventListener('click', function () {
+    audioPlayer.volume = 0;
+    this.style.display = 'none';
+    unmute.style.display = 'block';
+});
+
+unmute.addEventListener('click', function () {
+    audioPlayer.volume = 1;
+    this.style.display = 'none';
+    mute.style.display = 'block';
+});
+
+progress_bar_wrap.addEventListener('click', function (e) {
+    // คำนวณตำแหน่งที่คลิก
+    var progressWidth = progress_bar_wrap.offsetWidth; // ความกว้างของ progress bar
+    var clickPosition = e.offsetX; // ตำแหน่งที่คลิกจากทางซ้าย
+
+    // คำนวณตำแหน่งเวลาในเพลงที่คลิก
+    var newTime = (clickPosition / progressWidth) * audioPlayer.duration;
+
+    // ตั้งเวลาเพลงตามตำแหน่งที่คลิก
+    audioPlayer.currentTime = newTime;
+});
+
+
+var size = progress_bar_wrap.offsetWidth;
+setInterval(function () {
+    var width = parseInt(
+        (audioPlayer.currentTime * size) / audioPlayer.duration
+    );
+    progress_bar.style.width = width + 'px';
+}, 100);
+
+const lines = song.lyrics.trim().split('\n');
+
+const data = [];
+
+lines.map(function (line) {
+    const [time, lyric] = line.split('|');
+    data.push({
+        time: time,
+        lyric: lyric,
+    });
+});
+
+audioPlayer.addEventListener('timeupdate', function () {
+    data.forEach((item) => {
+        if (audioPlayer.currentTime >= item.time) {
+            lyrics.innerText = item.lyric;
+        }
+    });
+});
+
+audioPlayer.addEventListener('ended', function() {
+    pause.style.display = 'none';
+    play.style.display = 'block';
+    clearInterval(backgroundInterval);
+});
